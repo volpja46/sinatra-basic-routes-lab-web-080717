c0129e08e3a90fdaa2e66468240b7e4d7f82f8d0
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-08-28 14:21:16 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+      "Hello World"
+    end
+
+   get '/name' do
+  "My name is Jen"
+    end
+
+  get '/hometown' do
+    "My hometown is Carmel"
+    end
+
+   get '/favorite-song' do
+     "My favorite song is yaknow"
+    end
+
+
 end
